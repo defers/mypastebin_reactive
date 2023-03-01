abaa210ee00cd85709aec8c662e9d907f38507fa
Алексей <defers86@gmail.com>
2023-03-01 15:14:03 +0300

    01032023

--- src/main/java/com/defers/mypastebin/MypastebinApplication.java
+++ src/main/java/com/defers/mypastebin/MypastebinApplication.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.web.reactive.config.EnableWebFlux;
@@ -16,7 +15,6 @@ import org.springframework.web.reactive.config.EnableWebFlux;
         description = "Documentation APIs v1.0")
 )
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-@EnableCaching
 @EnableR2dbcRepositories
 @EnableWebFlux
 public class MypastebinApplication {
--- src/main/java/com/defers/mypastebin/dto/ApiResponse.java
+++ src/main/java/com/defers/mypastebin/dto/ApiResponse.java
@@ -1,11 +1,15 @@
 package com.defers.mypastebin.dto;
 
 import com.defers.mypastebin.enums.ApiResponseStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponse<T> {
     private ApiResponseStatus status;
     private T responseData;
